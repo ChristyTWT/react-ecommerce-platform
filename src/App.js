b01bb8d0dc0d52c905e3abdcec357aa6612779c2
mccbabii <mi2cn8cs@gmail.com>
2023-12-18 16:12:23 -0500

    Updated SCSS files for App and Home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Cart from "./components/cart/Cart";
+import Account from "./components/account/Account";
+import Products from "./components/products/Products";
+import Home from "./components/home/Home";
+import Contact from "./components/contact/Contact";
+import Header from "./components/header/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
